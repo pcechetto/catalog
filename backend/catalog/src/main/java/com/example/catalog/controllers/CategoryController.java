c57a19afeb72c6034955d736f803ab73ca9ff0f3
pcechetto <pedrocechetto@hotmail.com>
2025-04-02 15:38:42 -0300

    H2 database

---
+++ backend/catalog/src/main/java/com/example/catalog/controllers/CategoryController.java
@@ -0,0 +1,27 @@
+package com.example.catalog.controllers;
+
+import com.example.catalog.entities.Category;
+import com.example.catalog.services.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> categories = categoryService.findAll();
+        return ResponseEntity.ok().body(categories);
+    }
+}
---
+++ backend/catalog/src/main/java/com/example/catalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.example.catalog.services;
+
+import com.example.catalog.entities.Category;
+import com.example.catalog.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+}
