bd2b799d8a2b396693aeea1cb68d37df454271fe
pcechetto <pedrocechetto@hotmail.com>
2025-04-02 15:52:19 -0300

    database seeding

--- backend/catalog/src/main/java/com/example/catalog/services/CategoryService.java
+++ backend/catalog/src/main/java/com/example/catalog/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.example.catalog.services;
 import com.example.catalog.entities.Category;
 import com.example.catalog.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return categoryRepository.findAll();
     }
---
+++ backend/catalog/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into tb_category (name) values ('Livros');
+insert into tb_category (name) values ('Eletrônicos');
+insert into tb_category (name) values ('Computadores');
